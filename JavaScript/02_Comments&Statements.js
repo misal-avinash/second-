63d30ec627ca8a6a382223270571725db7f17b91
Misal Avinash <misal.avinash@cgi.com>
2017-02-20 11:56:42 +0530

    snarky comment

--- JavaScript/02_Comments&Statements.js
+++ JavaScript/02_Comments&Statements.js
@@ -6,3 +6,4 @@ document.write("i love ham");
 document.write("and i love bacon too");
 document.write("hey! me three");
 document.write("yeaahhh.. count me out");
+document.write("found the vegan");
