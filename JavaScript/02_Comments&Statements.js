a94c509cea961515e418720cd214230c95b6e7d8
Misal Avinash <misal.avinash@cgi.com>
2017-02-20 11:18:20 +0530

    js files

---
+++ JavaScript/02_Comments&Statements.js
@@ -0,0 +1,8 @@
+// This is a single-line comment.
+
+/* This is a multi-line comment */
+
+document.write("i love ham");
+document.write("and i love bacon too");
+document.write("hey! me three");
+document.write("yeaahhh.. count me out");
